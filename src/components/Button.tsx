873b3737d07bf2dd1983459592489ad84d98111c
Thomas Martinez <tmartinez114@gmail.com>
2024-05-26 00:26:53 -0400

    set button color by default and set the color interface to allow only the three we told

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,11 @@ import React from "react"
 
 interface Props {
   children: string
-  color: string
+  color?: "primary" | "secondary" | "danger"
   onClick: () => void
 }
 
-const Button = ({ children, color, onClick }: Props) => {
+const Button = ({ children, color = "primary", onClick }: Props) => {
   return (
     <button className={"btn btn-" + color} onClick={onClick}>
       {children}
